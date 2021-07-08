9675a9fc88bf05aa40457aaeffcefa64f3e13017
flexchemist <86171218+flexchemist@users.noreply.github.com>
2021-07-08 15:25:55 +0300

    heaader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Switch } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/Header'
 import HomePage from './views/homepage/HomePage'
-import ShopPage from "./views/shop/Shop"
-
+import ShopPage from './views/shop/Shop'
 
 function App() {
   return (
     <div>
       <main>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
